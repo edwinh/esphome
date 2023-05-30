a9125214efffa30406fd2100790507862fa621d4
edwinh <edwinhoogervorst@gmail.com>
2023-05-30 16:23:59 +0200

    Various changes

--- modules/kaku.h
+++ modules/kaku.h
@@ -4,7 +4,7 @@
 
 */
 namespace constants {
-  const std::vector<std::vector<int>> TELEGRAMS_ARRAY   = {{375, -1125, 375, -1125}, {}, {375, -1125, 1125, -375}};
+  const std::vector<std::vector<int>> TELEGRAMS         = {{375, -1125, 375, -1125}, {}, {375, -1125, 1125, -375}};
   const std::vector<int>              END_SEQUENCE      = {375, -11625};
   const std::vector<int>              D1_ON_SEQUENCE    = {2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2};
   const std::vector<int>              D1_OFF_SEQUENCE   = {2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0};
@@ -19,7 +19,7 @@ std::vector<int> kakuOldProtocol(const char* address, bool on) {
   //std::vector<std::vector<int>> telegramsArray = constants::TELEGRAMS_ARRAY;
   std::vector<int> result = {};
   for (const auto& element : kaku_sequence) {
-    result.insert(result.end(), constants::TELEGRAMS_ARRAY[element].begin(), constants::TELEGRAMS_ARRAY[element].end());
+    result.insert(result.end(), constants::TELEGRAMS[element].begin(), constants::TELEGRAMS[element].end());
   }
   result.insert(result.end(), constants::END_SEQUENCE.begin(), constants::END_SEQUENCE.end());
   return result;

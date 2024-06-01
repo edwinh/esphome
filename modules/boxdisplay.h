c4431fc1ee07c5076710fcfd8c0fbfc4fb24bcc0
edwinh <edwinhoogervorst@gmail.com>
2024-06-01 14:51:59 +0200

    Change kW to W on box

--- modules/boxdisplay.h
+++ modules/boxdisplay.h
@@ -101,7 +101,7 @@ void firstPage(esphome::display::Display& it) {
   line = 2;
   if (!isnan(esphome::id(current_power).state)) {
     it.image(margin_left, margin_top+(vertical_spacing*line), &esphome::id(flash_icon_small), textColor);
-    it.printf(margin_left + horizontal_spacing, text_vertical_padding+margin_top+(vertical_spacing*line), &esphome::id(font_small), textColor, "%.3f kW", esphome::id(current_power).state);
+    it.printf(margin_left + horizontal_spacing, text_vertical_padding+margin_top+(vertical_spacing*line), &esphome::id(font_small), textColor, "%.0f W", esphome::id(current_power).state * 1000);
   }
 };
 
